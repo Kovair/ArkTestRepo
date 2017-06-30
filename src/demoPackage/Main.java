9476e8d74be35b618a8f5505a78bc90fce1cc47d
ad243 <ad243@demo1.sr.kovair.com>
2017-06-30 00:20:52 -0700

    56885841

--- src/demoPackage/Main.java
+++ src/demoPackage/Main.java
@@ -2,6 +2,6 @@ package demoPackage;
 
 public class Main {
 	public static void main(String []args) {
-		System.out.println("Hello World"); // comment 30062017_002
+		System.out.println("Hello World"); // 56885841
 	}
 }
