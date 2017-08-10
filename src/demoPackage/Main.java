03c1ea28f4c1f63ad5071c1e325c0bfafc1989e9
ad243 <ad243@demo1.sr.kovair.com>
2017-08-10 00:30:22 -0700

    412

--- src/demoPackage/Main.java
+++ src/demoPackage/Main.java
@@ -2,6 +2,6 @@ package demoPackage;
 
 public class Main {
 	public static void main(String []args) {
-		System.out.println("Hello World"); // check Ark test
+		System.out.println("Hello World"); // check in Ark 412 
 	}
 }
